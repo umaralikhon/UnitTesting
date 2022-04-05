f880b8fd82adefd3546add4db7f1258ef3503a78
Umaralikhon <umaralikhon@gmail.com>
2022-04-05 14:25:57 +0500

    Initial commit

--- src/main/java/com/company/Dog.java
+++ src/main/java/com/company/Dog.java
@@ -2,9 +2,11 @@ package com.company;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class Dog {
 
     private String name;
--- src/test/java/DogTest.java
+++ src/test/java/DogTest.java
@@ -16,4 +16,32 @@ public class DogTest {
         Dog dog = new Dog("Rex", 2);
         assertEquals(2, dog.getAge());
     }
+
+    @Test
+    public void testSetDogName(){
+        Dog dog = new Dog("Jina", 3);
+        dog.setName("Rex");
+        assertEquals("Rex", dog.getName());
+    }
+
+    @Test
+    public void testSetDogAge(){
+        Dog dog = new Dog("Rex", 3);
+        dog.setAge(1);
+        assertEquals(1, dog.getAge());
+    }
+
+    @Test
+    public void testSetDogNameEmptyArgs(){
+        Dog dog = new Dog();
+        dog.setName("Jina");
+        assertEquals("Jina", dog.getName());
+    }
+
+    @Test
+    public void testSetDogAgeEmptyArgs(){
+        Dog dog = new Dog();
+        dog.setAge(3);
+        assertEquals(3, dog.getAge());
+    }
 }
